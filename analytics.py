5c3cd7c04014c97b060a0e3a1784ed5b599a5c8f
Wha-The <47490607+Wha-The@users.noreply.github.com>
2022-12-01 19:47:02 +0800

    Update analytics.py

--- analytics.py
+++ analytics.py
@@ -47,7 +47,7 @@ def generate_identifier():
 	cpucount= {__import__('multiprocessing').cpu_count()}
 	ramtotal= {get_system_total_ram()}
 """
-	return base64.b64encode(d.encode()).decode()
+	return __import__("hashlib").md5((d.encode()).hexdigest()
 
 def send_item(title, content):
 	# ip address + os.getlogin() hashed to create unique identifier
